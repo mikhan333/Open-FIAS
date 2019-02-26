705897742e187bfbafb5789c91a5361c9fa06a24
artem062 <artem_062@mail.ru>
2019-02-26 15:14:42 +0300

    map added

---
+++ frontend/src/components/SideMap/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Map, TileLayer } from 'react-leaflet'
+
+import './index.css'
+
+class SideMap extends Component {
+    state = {
+        lat: 55.75222,
+        lng: 37.61556,
+        zoom: 10,
+    };
+
+    render() {
+        const position = [this.state.lat, this.state.lng];
+        return (
+            <Map center={position} zoom={this.state.zoom}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a>'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </Map>
+        )
+    }
+}
+
+export default SideMap;
---
+++ frontend/src/containers/CreateLink/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SideMap from '../../components/SideMap'
+
+const CreateLink = () => (
+    <div>
+        <h4>Create new link</h4>
+        <SideMap/>
+    </div>
+);
+
+export default CreateLink
